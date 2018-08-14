ce8899a6c228e7a000611c83610df3a9efbb0613
cohji <koji.suzuki.cc@gmail.com>
2018-08-14 23:54:45 +0900

    add config page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+// import { AboutPage } from '../pages/about/about';
+import { ConfigPage } from '../pages/config/config';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -11,10 +12,12 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { IBeacon } from '@ionic-native/ibeacon';
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    ConfigPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -26,7 +29,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    ConfigPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -34,6 +37,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    IBeacon,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
